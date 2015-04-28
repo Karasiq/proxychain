91458aad3809c5413aa33bf9904b544adfe4ec52
Piston <yoba123@yandex.ru>
2015-04-28 22:36:00 +0300

    Moved proxy connectors to library See https://github.com/Karasiq/proxyutils

--- src/main/scala/com/karasiq/parsers/ParserException.scala
+++
@@ -1,6 +0,0 @@
-package com.karasiq.parsers
-
-/**
- * Generic parser exception
- */
-class ParserException(message: String = null, cause: Throwable = null) extends Exception(message, cause)
--- src/main/scala/com/karasiq/proxy/ProxyException.scala
+++
@@ -1,8 +0,0 @@
-package com.karasiq.proxy
-
-import java.io.IOException
-
-/**
- * Proxy connection error
- */
-class ProxyException(message: String = null, cause: Throwable = null) extends IOException(message, cause)
